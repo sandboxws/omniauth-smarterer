52ceb07bc8df5b8f892befd1e7127705f48d439f
Ahmed El.Hussaini <aelhussaini@gmail.com>
2013-01-09 04:30:47 +0200

    first working version

--- lib/omniauth/strategies/smarterer.rb
+++ lib/omniauth/strategies/smarterer.rb
@@ -1,34 +1,55 @@
-require 'multi_xml'
-require 'omniauth-oauth'
-require 'oauth'
+require 'omniauth-oauth2'
+require "multi_json"
+require "rest-client"
 
 module OmniAuth
   module Strategies
-    class Smarterer < OmniAuth::Strategies::OAuth
+    class Smarterer < OmniAuth::Strategies::OAuth2
+
       option :client_options, {
-        :site => 'http://smarterer.com',
-        :authorize_url => 'https://smarterer.com/oauth/authorize',
-        :token_url => 'https://smarterer.com/oauth/access_token'
+        site: 'https://smarterer.com/',
+        authorize_url: 'https://smarterer.com/oauth/authorize',
+        token_url: 'https://smarterer.com/oauth/access_token',
+        token_method: :get
       }
+
+      option :provider_ignores_state, true
 
       def request_phase
         super
       end
 
-      uid { raw_info['id'].to_s }
+      def callback_phase
+        super
+      end
+
+      uid { raw_info['username'].to_s }
 
       info do
         {
-          'name' => raw_info['name']
+          username: raw_info['username'],
+          first_name: raw_info['first_name'],
+          last_name: raw_info['last_name'],
+          full_name: "#{raw_info['first_name']} #{raw_info['last_name']}",
+          bio: raw_info['bio'],
+          image: raw_info['profile_image'],
+          url: raw_info['link'],
+          location: raw_info['location'],
+          organization: raw_info['organization']
         }
       end
 
       extra do
-        {:raw_info => raw_info}
+        {raw_info: raw_info}
       end
 
       def raw_info
         if @raw_info.nil?
+          @raw_info = {}
+          params = {access_token: access_token.token}
+          response = RestClient.get('https://smarterer.com/api/users/me', { :params => params })
+          user = MultiJson.decode(response.to_s)
+          @raw_info.merge!(user)
         end
 
         @raw_info
